563f9429d7bc4ebaa31fb199192aa9c82e01b672
Bass, David <d-bass@outlook.com>
2021-11-15 11:14:00 -0500

    New Namespace

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -1,15 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace OOPClass
-{
     class EntryPoint
     {
-        static void Main(string[] args)
-        {
+        static void Main()
+        { 
+        
+        // going to make an 'instance' of the 'Point Class'
+
+            Point point = new Point();
+
+            // assigning values using '.' dot notation
+
+            // I use my new ' point object' to access these properies in the 'Point' Class
+
+            point.x = 5;
+            point.y = 3;
+
+            System.Console.WriteLine(point.x);
+
+            System.Console.WriteLine(point.y);
+
+
         }
     }
-}
+
---
+++ PointsAndLines/Point.cs
@@ -0,0 +1,9 @@
+﻿//Adding a 'Namespace' and the folder name that containes the class 'PoinrAndLines'
+namespace PointAndLines
+
+// Adding a new class called Point
+class Point
+{
+    public int x;
+    public int y;
+}
