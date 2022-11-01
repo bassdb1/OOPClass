0512d09ce0c156e5f50fd50935ee618617265dfd
David Bass <davidbass@rtonational.com>
2022-11-01 09:37:08 -0400

    Add files via upload

---
+++ password_check/password_check/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace password_check
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblPassWord = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LblPassWord
+            // 
+            this.LblPassWord.AutoSize = true;
+            this.LblPassWord.Location = new System.Drawing.Point(50, 85);
+            this.LblPassWord.Name = "LblPassWord";
+            this.LblPassWord.Size = new System.Drawing.Size(73, 20);
+            this.LblPassWord.TabIndex = 1;
+            this.LblPassWord.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(143, 85);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(291, 27);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(96, 171);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 29);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Check";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(359, 171);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(94, 29);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(71, 244);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox3.Size = new System.Drawing.Size(425, 135);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "Password must have the following:\r\n\r\nAt least 8 Characters in Length\r\n1 Uppercase" +
+    " character\r\n1 Lowercase charcter\r\n1 Special character (!@#$%^&*)\r\n";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 403);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.LblPassWord);
+            this.Name = "Form1";
+            this.Text = "Password Verifier";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label LblPassWord;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ password_check/password_check/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace password_check
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Method to test if our password contains a number
+
+        private int numberPass(string pass)
+        {
+            int num = 0;
+
+            foreach (char ch in pass)
+            {
+                if (char.IsDigit(ch))
+                {
+                    num++;
+                }
+            }
+            return num;
+        }
+
+        // Method to test if our password contains an Uppercase character
+
+        private int upperCase(string pass)
+        {
+            int num = 0;
+
+            foreach (char ch in pass)
+            {
+                if (char.IsUpper(ch))
+                {
+                    num++;
+                }
+            }
+            return num;
+        }
+
+        // Method to test if our password contains an lowercase character
+
+        private int lowerCase(string pass)
+        {
+            int num = 0;
+
+            foreach (char ch in pass)
+            {
+                if (char.IsLower(ch))
+                {
+                    num++;
+                }
+            }
+            return num;
+        }
+
+        // Method to test if our password contains an special character
+
+        private int specialCharacter(string pass)
+        {
+            int num = 0;
+            const string SYMBOLS = "!@#$%^&*()";
+            foreach (var item in SYMBOLS)
+            {
+                if (pass.Contains(item))
+                {
+                    num++;
+                }
+            }
+            return num;
+
+        }
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            const int MIN_LENGTH = 8;
+
+            string password = txtPassword.Text;
+
+            if (password.Length >= MIN_LENGTH  && numberPass(password) >=1 && upperCase(password) >=1 && lowerCase(password) >=1 && specialCharacter(password) >=1)
+            {
+                MessageBox.Show("Password is good");
+
+            }
+            else
+            {
+                MessageBox.Show("Password is bad");
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
